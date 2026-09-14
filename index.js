e7599573ddfdb5745d97200c4ed551bbfc201655
Ahrar <ahrarahmed550@gmail.com>
2026-09-14 15:41:56 +0530

    frontend deployment setup

--- index.js
+++ index.js
@@ -6,46 +6,45 @@ const app = express()
 
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
-morgan.token('body', (req) => { 
-    console.log(req.body, typeof req.body)
-    console.log(JSON.stringify(req.body), typeof JSON.stringify(req.body))
-    
+
+morgan.token('body', (req) => {
     return JSON.stringify(req.body)
- })
+})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/persons', (req, res) => { 
+app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
- 
 
-app.get('/api/info', (req, res) => { 
+
+app.get('/api/info', (req, res) => {
     const count = persons.length
     res.send(`
         <p>Phonebook has info for ${count} people</p>
@@ -60,21 +59,21 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 
-app.delete('/api/persons/:id', (req, res) => { 
+app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    
+
     persons = persons.filter(p => p.id !== id)
     res.status(204).end()
 })
- 
-const generateId = () => { 
+
+const generateId = () => {
     const rand = Math.floor(Math.random() * 1000000)
     return rand
- }
+}
 
-app.post('/api/persons', (req, res) => { 
+app.post('/api/persons', (req, res) => {
     const body = req.body
-    
+
     if (!body.name || !body.number) {
         return res.status(400).json({
             error: 'missing name or number'
@@ -86,20 +85,20 @@ app.post('/api/persons', (req, res) => {
             error: 'name must be unique'
         })
     }
-        const person = {
-            name: body.name,
-            number: body.number,
-            id: generateId()
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
     }
 
     persons = persons.concat(person)
     res.json(person)
 })
- 
+
 
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => { 
-    console.log(`Server is running on port ${PORT}`) 
- })
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
