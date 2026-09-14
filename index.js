9ac45a908c70b1d9f514a6289be4261d215f0e46
Ahrar <ahrarahmed550@gmail.com>
2026-09-14 16:14:13 +0530

    Configure frontend proxy for local backend & remove cors dependency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+
 
 const app = express()
 
@@ -29,7 +29,6 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 
 
